c63ecd46d6b3d0216fd86f1073294571661ba64a
Leandro B. Andrade <leandrobe.andrade@gmail.com>
2024-03-28 00:06:01 -0300

    EcmaScript

--- ecmascript6/curso1/scripts/03.js
+++ ecmascript6/curso1/scripts/03.js
@@ -1,26 +1,25 @@
-const data = [ 
+const data = [
   {
-		name: 'Willian',
-		age: 26,
-		city: 'Dublin'
-	},
-	{
-		name: 'Jonas',
-		age: 22,
-		city: 'Cologne'
-	}
+    name: 'Willian',
+    age: 26,
+    city: 'Dublin'
+  },
+  {
+    name: 'Jonas',
+    age: 22,
+    city: 'Cologne'
+  }
 ];
 
+const jonas = data.find(person => person.name === 'Jonas');
+const jonasIndex = data.findIndex(person => person.name === 'Jonas');
+console.log(jonas);
 
 const sampleArray = [4, -5, 0, -1, 2];
 const underZero = sampleArray.find(x => x > 0);
 const underZeroIndex = sampleArray.findIndex(x => x < 0);
 console.log(underZero);
 console.log(underZeroIndex);
-
-const jonas = data.find(person => person.name === 'Jonas');
-const jonasIndex = data.findIndex(person => person.name === 'Jonas');
-console.log(jonas);
 
 const arr = new Array(50);
 arr.fill('lol', 3, 6);
