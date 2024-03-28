62e8fdf08da7d9be7f837b701a03ca8d7abfac0c
Leandro B. Andrade <leandrobe.andrade@gmail.com>
2024-03-28 00:12:45 -0300

    EcmaScript

--- ecmascript6/curso1/scripts/07.js
+++ ecmascript6/curso1/scripts/07.js
@@ -13,22 +13,22 @@ const dog = new Animal('dog', 'auau');
 const cat = new Animal('cat', 'meow');
 
 class Animals {
-    constructor(kind, sound) {
-        this.kind = kind;
-        this.sound = sound;
-    }
-    hello() {
-        console.log(`${this.sound} I'm a ${this.kind}!`);
-    }
-    static info() {
-        console.log('This is a class to create animals!');
-    }
-    get name() {
-        console.log(`${this.sound} My name is Jake!`);
-    }
-    set nickname(nick) {
-        this.nick = nick;
-    }
+  constructor(kind, sound) {
+    this.kind = kind;
+    this.sound = sound;
+  }
+  hello() {
+    console.log(`${this.sound} I'm a ${this.kind}!`);
+  }
+  static info() {
+    console.log('This is a class to create animals!');
+  }
+  get name() {
+    console.log(`${this.sound} My name is Jake!`);
+  }
+  set nickname(nick) {
+    this.nick = nick;
+  }
 }
 const cao = new Animals('cao', 'auau');
 const gato = new Animals('gato', 'meow');
