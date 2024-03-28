6ee37021458bd28d4132b668f5c05c78f2d5398d
Leandro B. Andrade <leandrobe.andrade@gmail.com>
2024-03-28 00:10:35 -0300

    EcmaScript

--- ecmascript6/curso1/scripts/06.js
+++ ecmascript6/curso1/scripts/06.js
@@ -1,37 +1,37 @@
-var defer = new Promise((resolve, reject) => { 
-	setTimeout(() => {
-		if (true) resolve('Hello! It works!');
-		else reject('Error!');
-	}, 2000);
+var defer = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (true) resolve('Hello! It works!');
+    else reject('Error!');
+  }, 2000);
 });
 
 defer
 .then((data) => {
-	console.log(data);
-	return 'foo';
+  console.log(data);
+  return 'foo';
 })
 .then((data) => console.log(data))
 .catch((err) => console.log(err));
 
 const currency = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve({ currency: 'euro', value: 3.50 });
-	}, 5000);
+  setTimeout(() => {
+    resolve({ currency: 'euro', value: 3.50 });
+  }, 5000);
 });
 
 const countries = new Promise((resolve, reject) => {
-	resolve(['Ireland', 'England', 'Scotland']);
+  resolve(['Ireland', 'England', 'Scotland']);
 });
 
 
 /* Promise
 .all([currency, countries])
 .then(responses => {
-    console.log(responses);
+  console.log(responses);
 }) */
 
 Promise
 .race([currency, countries])
 .then(responses => {
-	console.log(responses);
+  console.log(responses);
 })
